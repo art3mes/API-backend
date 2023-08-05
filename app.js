2116df9a74a64b336607faa221868b729d7a85b2
art3mes <93867313+art3mes@users.noreply.github.com>
2023-08-05 22:13:51 +0530

    setup

--- app.js
+++ app.js
@@ -1,1 +1,19 @@
-console.log("hi");
+const express = require("express");
+const cors =require ("cors");           //to enable access from multiple domains
+const bodyParser =require("body-parser");
+require('dotenv').config();
+    
+const app=express();
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended:true}));
+
+const corsOptions ={
+    origin:'*',
+    credentials:true,
+    optionSuccessStatus:200
+}
+app.use(cors(corsOptions));
+
+app.listen(process.env.ROOT||4000,function(){
+    console.log("server is running");
+});
