b749076bc913645e07ae8b178bfa35649cb75e27
art3mes <93867313+art3mes@users.noreply.github.com>
2023-08-11 21:24:01 +0530

    weather api is working

--- app.js
+++ app.js
@@ -26,24 +26,15 @@ app.post("/", function(req,res){
     const metric="metric";
 
     const url="https://api.openweathermap.org/data/2.5/weather?q="+query+"&appid="+appKey+"&units="+metric;
-    https.get(url,function(response){          
-        //console.log(response.statusCode);                                                               
+    https.get(url,function(response){                                                                       
         response.on("data",function(data){                  
             const weatherDATA= JSON.parse(data);      
-            // const temp=weatherDATA.main.temp;
-            // const desc=weatherDATA.weather[0].description;
             const icon=weatherDATA.weather[0].icon;
-            // const imageURL="http://openweathermap.org/img/wn/"+icon+"@2x.png";
             const DATA = {
                 temp:weatherDATA.main.temp,
                 desc: weatherDATA.weather[0].description,
                 imageURL:"http://openweathermap.org/img/wn/"+icon+"@2x.png"
             };
-            
-            // res.write("<h1>Current temperature in "+query+" is "+temp+" degrees Celcius</h1>");
-            // res.write("<p>The current weather description is "+desc+".<p>");
-            // res.write("<img src="+imageURL+">");
-
             res.send(DATA);
         });
     });
